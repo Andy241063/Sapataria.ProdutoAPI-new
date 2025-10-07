e47f05b18679545ae7265508e62869e8cf6fcd59
Andre Santos <andrefonseca.info@gmail.com>
2025-10-07 17:39:23 -0300

    Creating new UseCase and Interface

---
+++ Sapataria.ProdutoAPI.Application/Impl/ResisterProductsUseCase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Sapataria.ProdutoAPI.Application.Infrastructure.Interfaces;
+using Sapataria.ProdutoAPI.Domain.Entities;
+
+namespace Sapataria.ProdutoAPI.Application.Impl
+{
+    internal class ResisterProductsUseCase : IResisterProductsUseCase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ResisterProductsUseCase(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public void SetProducts(Produto produto)
+        {
+            _productRepository.Save(produto);
+        }
+
+        public IEnumerable<Produto> SetProducts()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    }
+}
---
+++ Sapataria.ProdutoAPI.Application/Infrastructure.Interfaces/IResisterProductsUseCase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Sapataria.ProdutoAPI.Domain.Entities;
+
+namespace Sapataria.ProdutoAPI.Application.Infrastructure.Interfaces
+{
+    public interface IResisterProductsUseCase
+    {
+        IEnumerable<Produto> SetProducts();
+    }
+}
