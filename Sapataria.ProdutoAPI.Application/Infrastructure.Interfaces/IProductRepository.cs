09c460a8ccca0612de512b618b89f43f1849201c
Andre Santos <andrefonseca.info@gmail.com>
2025-10-28 17:11:54 -0300

    Connecting Database

--- Sapataria.ProdutoAPI.Application/Infrastructure.Interfaces/IProductRepository.cs
+++ Sapataria.ProdutoAPI.Application/Infrastructure.Interfaces/IProductRepository.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using Sapataria.ProdutoAPI.Domain.Entities;
+using MySql.Data.MySqlClient;
+using System.Linq.Expressions;
 
 namespace Sapataria.ProdutoAPI.Application.Infrastructure.Interfaces
 {
--- Sapataria.ProdutoAPI.Infrastructure/Impl/ProductRepository.cs
+++ Sapataria.ProdutoAPI.Infrastructure/Impl/ProductRepository.cs
@@ -1,11 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using MySql.Data.MySqlClient;
 using Sapataria.ProdutoAPI.Application.Infrastructure.Interfaces;
 using Sapataria.ProdutoAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace Sapataria.ProdutoAPI.Infrastructure.Impl
 {
     internal class ProductRepository : IProductRepository
     {
+        public class DatabaseConnection
+        {
+                public static void Connect()
+                {
+                    string connectionString = "server=127.0.0.1;database=sua_base;uid=root;pwd=sua_senha;";
+                    using (MySqlConnection connection = new MySqlConnection(connectionString))
+                    {   
+                        // Por que utilizar using na criacao da instancia ?
+                        try
+                        {
+                            connection.Open();
+                            Console.WriteLine("Conexão bem-sucedida!");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Erro ao conectar: " + ex.Message);
+                        }
+                    }
+                }
+        }
+
         private List<Produto> _produtos;
        
         public ProductRepository()
