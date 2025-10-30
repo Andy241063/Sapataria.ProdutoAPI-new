e817a99e226e776985509be698c579db8d4c73b2
Andy241063 <andrefonseca.info@gmail.com>
2025-10-30 19:43:29 -0300

    chore: alteracoes da aula

--- Sapataria.ProdutoAPI.Infrastructure/Impl/ProductRepository.cs
+++ Sapataria.ProdutoAPI.Infrastructure/Impl/ProductRepository.cs
@@ -8,14 +8,18 @@ namespace Sapataria.ProdutoAPI.Infrastructure.Impl
 {
     internal class ProductRepository : IProductRepository
     {
-        string connectionString = "server=MyLocalHost;database=sapataria;uid=root";
+        string connectionString = "Server=localhost;Port=3306;Database=sapataria;User ID=root;Password=;";
         
         private List<Produto> _produtos;
        
         public ProductRepository()
-
         {
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            _produtos = new List<Produto>();
+        }
+
+        public IEnumerable<Produto> Get()
+        {
+            using MySqlConnection connection = new MySqlConnection(connectionString);
 
             try
             {
@@ -27,46 +31,29 @@ namespace Sapataria.ProdutoAPI.Infrastructure.Impl
                 Console.WriteLine("Erro ao conectar: " + ex.Message);
             }
 
-            _produtos = new List<Produto>();
-        }
-
-        public IEnumerable<Produto> Get()
-        {
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-
-                try
-                {
-                    connection.Open();
-                    Console.WriteLine("Conexão bem-sucedida!");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Erro ao conectar: " + ex.Message);
-                }
-
             return _produtos;
         }
 
         public void Save(Produto produto)
         {
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            using MySqlConnection connection = new MySqlConnection(connectionString);
 
-                try
-                {
-                    connection.Open();
-                    Console.WriteLine("Conexão bem-sucedida!");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Erro ao conectar: " + ex.Message);
-                }
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Conexão bem-sucedida!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao conectar: " + ex.Message);
+            }
 
             _produtos.Add(produto);
         }
 
         public void Update(Produto produto, int id)
         {
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            using MySqlConnection connection = new MySqlConnection(connectionString);
 
             try
             {
@@ -81,17 +68,17 @@ namespace Sapataria.ProdutoAPI.Infrastructure.Impl
 
         public void Delete(int id)
         {
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            using MySqlConnection connection = new MySqlConnection(connectionString);
 
-                try
-                {
-                    connection.Open();
-                    Console.WriteLine("Conexão bem-sucedida!");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Erro ao conectar: " + ex.Message);
-                }
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Conexão bem-sucedida!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao conectar: " + ex.Message);
+            }
         }
     }
 }
