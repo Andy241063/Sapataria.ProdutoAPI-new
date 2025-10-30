57bcb5c39b393ac0785b6c9bcd2fb5737aac5d5b
Andre Santos <andrefonseca.info@gmail.com>
2025-10-30 17:50:54 -0300

    Connection with Database

--- Sapataria.ProdutoAPI.Infrastructure/Impl/ProductRepository.cs
+++ Sapataria.ProdutoAPI.Infrastructure/Impl/ProductRepository.cs
@@ -1,5 +1,4 @@
-﻿using MySql.Data.MySqlClient;
-using Sapataria.ProdutoAPI.Application.Infrastructure.Interfaces;
+﻿using Sapataria.ProdutoAPI.Application.Infrastructure.Interfaces;
 using Sapataria.ProdutoAPI.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -9,50 +8,90 @@ namespace Sapataria.ProdutoAPI.Infrastructure.Impl
 {
     internal class ProductRepository : IProductRepository
     {
-        public class DatabaseConnection
-        {
-                public static void Connect()
-                {
-                    string connectionString = "server=127.0.0.1;database=sua_base;uid=root;pwd=sua_senha;";
-                    using (MySqlConnection connection = new MySqlConnection(connectionString))
-                    {   
-                        // Por que utilizar using na criacao da instancia ?
-                        try
-                        {
-                            connection.Open();
-                            Console.WriteLine("Conexão bem-sucedida!");
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine("Erro ao conectar: " + ex.Message);
-                        }
-                    }
-                }
-        }
-
+        string connectionString = "server=MyLocalHost;database=sapataria;uid=root";
+        
         private List<Produto> _produtos;
        
         public ProductRepository()
+
         {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Conexão bem-sucedida!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao conectar: " + ex.Message);
+            }
+
             _produtos = new List<Produto>();
         }
 
         public IEnumerable<Produto> Get()
         {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Conexão bem-sucedida!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro ao conectar: " + ex.Message);
+                }
+
             return _produtos;
         }
 
         public void Save(Produto produto)
         {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Conexão bem-sucedida!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro ao conectar: " + ex.Message);
+                }
+
             _produtos.Add(produto);
         }
 
         public void Update(Produto produto, int id)
         {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Conexão bem-sucedida!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao conectar: " + ex.Message);
+            }
         }
 
         public void Delete(int id)
         {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Conexão bem-sucedida!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro ao conectar: " + ex.Message);
+                }
         }
     }
 }
